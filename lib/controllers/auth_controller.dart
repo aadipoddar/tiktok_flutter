362d9f308ed691e65f4eeabf458221bf5cad1410
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-20 18:12:04 +0530

    Persisting User State
    Navigate from Login to Home Screen

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -6,13 +6,32 @@ import 'package:image_picker/image_picker.dart';
 import 'package:tiktok_flutter/constants.dart';
 import 'package:get/get.dart';
 import 'package:tiktok_flutter/models/user.dart' as model;
+import 'package:tiktok_flutter/views/screens/auth/login_screen.dart';
+import 'package:tiktok_flutter/views/screens/home_screen.dart';
 
 class AuthController extends GetxController {
   static AuthController instance = Get.find();
 
+  late Rx<User?> _user;
   late Rx<File?> _pickedImage;
 
   File? get profilePhoto => _pickedImage.value;
+
+  @override
+  void onReady() {
+    super.onReady();
+    _user = Rx<User?>(firebaseAuth.currentUser);
+    _user.bindStream(firebaseAuth.authStateChanges());
+    ever(_user, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user == null) {
+      Get.offAll(() => LoginScreen());
+    } else {
+      Get.offAll(() => const HomeScreen());
+    }
+  }
 
   void pickImage() async {
     final pickedImage =
