ccad0a2ce274b32ed0c7c5f90dc4d5f0235f60dd
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-21 22:44:43 +0530

    Search Functionality
    Search Users

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,11 +4,12 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_flutter/controllers/auth_controller.dart';
 import 'package:tiktok_flutter/views/screens/add_video_screen.dart';
+import 'package:tiktok_flutter/views/screens/search_screen.dart';
 import 'package:tiktok_flutter/views/screens/video_screen.dart';
 
 List pages = [
   VideoScreen(),
-  Text('Search Screen'),
+  SearchScreen(),
   const AddVideoScreen(),
   Text('Messages Screen'),
   Text('Profile Screen')
