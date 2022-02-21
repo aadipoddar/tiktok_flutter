002987964a00e5dfc2af931ebc13f74dbe12ecd5
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-21 23:21:05 +0530

    Navigate From Login to Signup and Vice Versa

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_flutter/constants.dart';
+import 'package:tiktok_flutter/views/screens/auth/signup_screen.dart';
 import 'package:tiktok_flutter/views/widgets/text_input_field.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -11,76 +12,107 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            alignment: Alignment.center,
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text('Tik Tok',
-                  style: TextStyle(
-                      fontSize: 35,
-                      color: buttonColor,
-                      fontWeight: FontWeight.w900)),
-              const Text('Login',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700)),
-              const SizedBox(height: 25),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextInputField(
-                  controller: _emailController,
-                  labelText: 'Email',
-                  icon: Icons.email,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Tiktok Clone',
+              style: TextStyle(
+                fontSize: 35,
+                color: buttonColor,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _emailController,
+                labelText: 'Email',
+                icon: Icons.email,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _passwordController,
+                labelText: 'Password',
+                icon: Icons.lock,
+                isObscure: true,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 50,
+              decoration: BoxDecoration(
+                color: buttonColor,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(5),
                 ),
               ),
-              const SizedBox(height: 25),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextInputField(
-                    controller: _passwordController,
-                    labelText: 'Password',
-                    icon: Icons.lock,
-                    isObscure: true),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                  width: MediaQuery.of(context).size.width - 40,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: buttonColor,
-                      borderRadius: const BorderRadius.all(Radius.circular(5))),
-                  child: InkWell(
-                      onTap: () => authController.loginUser(
-                          _emailController.text, _passwordController.text),
-                      child: const Center(
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w700),
-                        ),
-                      ))),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Don\t have an account? ',
-                    style: TextStyle(fontSize: 20),
+              child: InkWell(
+                onTap: () => authController.loginUser(
+                  _emailController.text,
+                  _passwordController.text,
+                ),
+                child: const Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
-                  InkWell(
-                      onTap: () {
-                        //print('Navigating User');
-                      },
-                      child: Text(
-                        'Register',
-                        style: TextStyle(fontSize: 20, color: buttonColor),
-                      ))
-                ],
-              )
-            ])));
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Don\'t have an account? ',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                InkWell(
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => SignupScreen(),
+                    ),
+                  ),
+                  child: Text(
+                    'Register',
+                    style: TextStyle(fontSize: 20, color: buttonColor),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
